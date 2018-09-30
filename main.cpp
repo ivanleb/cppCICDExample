f833141c841b600df8a89c3ead1c48ac7bc88893
ivan leb <lebedkin.ivan.f@yandex.ru>
2018-09-30 23:55:12 +0300

    change main.cpp

--- main.cpp
+++ main.cpp
@@ -5,7 +5,9 @@
 int main()
 {
     auto logger = spdlog::stdout_logger_mt("console");
-    logger->info("version {} was started", version());
+    logger->info("build {}", version());
+
+    logger->info("Hello, World!");
 
     return 0;
 }
